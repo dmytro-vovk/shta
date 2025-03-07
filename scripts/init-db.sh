f01181dd4c47d807acc188e5b014b8f6fc05053e
Dmytro Vovk <dmitry.vovk@gmail.com>
2025-03-07 00:15:36 +0000

    fix: db access rights

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -5,25 +5,20 @@ set -e
 if [ -n "${APP_DB_USER}" ] && [ -n "${APP_DB_PASSWORD}" ] && [ -n "${APP_DB_NAME}" ]; then
   echo "Creating database '${APP_DB_NAME}' and user '${APP_DB_USER}'..."
 
-  psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -d "${POSTGRES_DB:-postgres}" <<-END
+  psql \
+    -v ON_ERROR_STOP=1 \
+    --username "${POSTGRES_USER}" \
+    -d "${POSTGRES_DB:-postgres}" <<-END
     CREATE USER ${APP_DB_USER} WITH PASSWORD '${APP_DB_PASSWORD}';
     CREATE DATABASE ${APP_DB_NAME} OWNER ${APP_DB_USER};
-
-    -- REVOKE CONNECT ON DATABASE ${APP_DB_NAME} FROM PUBLIC;
-    -- REVOKE ALL ON SCHEMA public FROM PUBLIC;
-    -- REVOKE ALL ON ALL TABLES IN SCHEMA public FROM PUBLIC;
+    GRANT ALL PRIVILEGES ON DATABASE ${APP_DB_NAME} TO ${APP_DB_USER};
 END
 
   echo "Creating tables in database '${APP_DB_NAME}'..."
 
-  psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -d "${APP_DB_NAME}" < /docker-entrypoint-initdb.d/init.sql
-
-  echo "Granting access to database '${APP_DB_NAME}' for user '${APP_DB_USER}'..."
-
-  psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" -d "${POSTGRES_DB:-postgres}" <<-END
-      GRANT CONNECT ON DATABASE ${APP_DB_NAME} TO ${APP_DB_USER};
-      GRANT USAGE ON SCHEMA public TO ${APP_DB_USER};
-      GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO ${APP_DB_USER};
-END
-
+  psql \
+    -v ON_ERROR_STOP=1 \
+    --username "${APP_DB_USER}" \
+    -d "${APP_DB_NAME}" \
+    < /docker-entrypoint-initdb.d/init.sql
 fi
